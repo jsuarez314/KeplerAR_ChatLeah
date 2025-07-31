26b0b713c23d065f92acfd3f25c4ce3f70cb072e
John Fredy Suárez Pérez <jf.suarez@tec.mx>
2025-07-31 00:39:59 -0600

    Feat: Added htmlTemplate

---
+++ htmlTemplates.py
@@ -0,0 +1,21 @@
+bot_template='''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/128/6989/6989363.png">
+    </div>
+    <br> Leah: </br>
+    <div class="message">{{MSG}}</div>
+    <p>
+ </div>   
+'''
+
+user_template='''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/128/921/921490.png">
+    </div>
+    <br> Centro Espacial (Tierra): </br>
+    <div class="message">{{MSG}}</div>
+    <p>
+ </div>   
+'''
